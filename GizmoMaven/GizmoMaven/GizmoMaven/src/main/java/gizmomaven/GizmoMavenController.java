89086c3f6e12a4c847429da0325128e7e36881ef
snehaamallick <snehamallick@yahoo.com>
2016-06-15 15:54:05 +0530

    User Product Page Created

---
+++ GizmoMaven/GizmoMaven/GizmoMaven/src/main/java/gizmomaven/GizmoMavenController.java
@@ -0,0 +1,38 @@
+package gizmomaven;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GizmoMavenController {
+
+	
+	@RequestMapping(value={"/","gotoindex"})
+	public String loadIndexPage()
+	{
+		return "index";
+	}
+	 
+	@RequestMapping("/aboutus")
+	public String loadAboutusPage()
+	{
+		return "aboutus";
+	}
+	@RequestMapping("/signup")
+	public String loadSignUpPage()
+	{
+		return "signup";
+	}
+	@RequestMapping("/Contact")
+	public String loadContactPage()
+	{
+		return "Contact";
+	}
+	@RequestMapping("/user_product_page")
+	public String loaduserproductpage()
+	{
+		return "user_product_page";
+	}
+}
